b24d10741b7a609ccaf68cab0b5393aa41b43623
himazin331 <sion20022013@gmail.com>
2021-04-28 19:24:16 +0900

    SRGAN prediction

--- srgan_pre.py
+++ srgan_pre.py
@@ -1,29 +1,28 @@
+import tensorflow as tf
+import tensorflow.keras.layers as kl
+
+import cv2
+from PIL import Image
+import numpy as np
+
 import argparse as arg
 import os
 import sys
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-import tensorflow as tf
-import tensorflow.keras.layers as kl
-
-import cv2
-from PIL import Image
-import matplotlib.pyplot as plt
-
-import numpy as np
 
 # Super-resolution Image Generator
 class Generator(tf.keras.Model):
     def __init__(self, input_shape):
         super().__init__()
 
-        input_shape_ps = (input_shape[0], input_shape[1], 64) 
+        input_shape_ps = (input_shape[0], input_shape[1], 64)
 
         # Pre stage(Down Sampling)
         self.pre = [
             kl.Conv2D(64, kernel_size=9, strides=1,
-                    padding="same", input_shape=input_shape),
+                        padding="same", input_shape=input_shape),
             kl.Activation(tf.nn.relu)
         ]
 
@@ -41,7 +40,7 @@ class Generator(tf.keras.Model):
         ]
 
         # Pixel Shuffle(Up Sampling)
-        self.ps =[
+        self.ps = [
             [
                 Pixel_shuffler(128, input_shape_ps) for _ in range(2)
             ],
@@ -49,7 +48,6 @@ class Generator(tf.keras.Model):
         ]
 
     def call(self, x):
-
         # Pre stage
         pre = x
         for layer in self.pre:
@@ -58,8 +56,8 @@ class Generator(tf.keras.Model):
         # Residual Block
         res = pre
         for layer in self.res:
-            for l in layer:
-                res = l(res)
+            for _layer in layer:
+                res = _layer(res)
         
         # Middle stage
         middle = res
@@ -71,12 +69,13 @@ class Generator(tf.keras.Model):
         out = middle
         for layer in self.ps:
             if isinstance(layer, list):
-                for l in layer:
-                    out = l(out)
+                for _layer in layer:
+                    out = _layer(out)
             else:
                 out = layer(out)
 
         return out
+
 
 # Pixel Shuffle
 class Pixel_shuffler(tf.keras.Model):
@@ -84,16 +83,16 @@ class Pixel_shuffler(tf.keras.Model):
         super().__init__()
 
         self.conv = kl.Conv2D(out_ch, kernel_size=3, strides=1,
-                            padding="same", input_shape=input_shape)
+                                padding="same", input_shape=input_shape)
         self.act = kl.Activation(tf.nn.relu)
-    
+
     # forward proc
     def call(self, x):
-
         d1 = self.conv(x)
         d2 = self.act(tf.nn.depth_to_space(d1, 2))
 
         return d2
+
 
 # Residual Block
 class Res_block(tf.keras.Model):
@@ -101,55 +100,54 @@ class Res_block(tf.keras.Model):
         super().__init__()
 
         self.conv1 = kl.Conv2D(ch, kernel_size=3, strides=1,
-                            padding="same", input_shape=input_shape)
+                                padding="same", input_shape=input_shape)
         self.bn1 = kl.BatchNormalization()
         self.av1 = kl.Activation(tf.nn.relu)
 
         self.conv2 = kl.Conv2D(ch, kernel_size=3, strides=1,
-                            padding="same")
+                                padding="same")
         self.bn2 = kl.BatchNormalization()
 
         self.add = kl.Add()
 
     def call(self, x):
-
         d1 = self.av1(self.bn1(self.conv1(x)))
         d2 = self.bn2(self.conv2(d1))
 
         return self.add([x, d2])
 
-def main():
 
+def main():
     # Command line option
     parser = arg.ArgumentParser(description='Super-resolution GAN prediction')
     parser.add_argument('--param', '-p', type=str, default=None,
-                        help='学習済みパラメータの指定(未指定ならエラー)')    
+                        help='Specify learned parameters (If not specified, an error)')
     parser.add_argument('--data_img', '-d', type=str, default=None,
-                        help='画像ファイルの指定(未指定ならエラー)')
+                        help='Specify an image file (If not specified, an error)')
     parser.add_argument('--out', '-o', type=str, default=os.path.join(os.path.dirname(os.path.abspath(__file__)), "result"),
-                        help='保存先指定(デフォルト値=./result)')                        
+                        help='Specify the destination (default: ./result)')
     parser.add_argument('--he', '-he', type=int, default=128,
-                        help='リサイズの高さ指定(デフォルト値=128)')      
+                        help='Resize height (default: 128)')
     parser.add_argument('--wi', '-wi', type=int, default=128,
-                        help='リサイズの指定(デフォルト値=128)')
+                        help='Resize width (default: 128)')
     parser.add_argument('--mag', '-m', type=int, default=2,
-                        help='縮小倍率の指定(デフォルト値=2)')                           
+                        help='Magnification (default: 2)')
     args = parser.parse_args()
 
     # Parameter-File not specified. -> Exception
-    if args.param == None:
+    if args.param is None:
         print("\nException: Trained Parameter-File not specified.\n")
         sys.exit()
     # An Parameter-File that does not exist was specified. -> Exception
-    if os.path.exists(args.param) != True:
+    if os.path.exists(args.param) is False:
         print("\nException: Trained Parameter-File {} is not found.\n".format(args.param))
         sys.exit()
     # Image not specified. -> Exception
-    if args.data_img == None:
+    if args.data_img is None:
         print("\nException: Image not specified.\n")
         sys.exit()
     # An image that does not exist was specified. -> Exception
-    if os.path.exists(args.data_img) != True:
+    if os.path.exists(args.data_img) is False:
         print("\nException: Image {} is not found.\n".format(args.data_img))
         sys.exit()
     # When 0 is entered for either width/height or Reduction ratio. -> Exception
@@ -182,13 +180,13 @@ def main():
     hr_img = cv2.resize(img, (args.he, args.wi))
 
     # Low-resolution Image
-    lr_img = cv2.resize(hr_img, (int(args.he/args.mag), int(args.wi/args.mag)))
+    lr_img = cv2.resize(hr_img, (int(args.he / args.mag), int(args.wi / args.mag)))
     lr_img = cv2.resize(lr_img, (args.he, args.wi))
     lr_img_s = lr_img
 
     # Image processing
-    lr_img = tf.convert_to_tensor(lr_img, np.float32) 
-    lr_img = tf.convert_to_tensor(lr_img, np.float32) 
+    lr_img = tf.convert_to_tensor(lr_img, np.float32)
+    lr_img = tf.convert_to_tensor(lr_img, np.float32)
     lr_img = (lr_img - 127.5) / 127.5
     lr_img = lr_img[np.newaxis, :, :, :]
 
@@ -214,6 +212,7 @@ def main():
     hr_img = Image.fromarray(np.uint8(hr_img))
     hr_img.show()
     hr_img.save(os.path.join(args.out, "High-resolution Image(SRGAN).bmp"))
-    
+
+
 if __name__ == "__main__":
     main()
