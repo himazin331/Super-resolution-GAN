8eb8c86638009663299a252380f844f045f64103
himazin331 <sion20022013@gmail.com>
2021-04-28 19:24:31 +0900

    SRGAN training

--- srgan_tr.py
+++ srgan_tr.py
@@ -1,9 +1,3 @@
-import argparse as arg
-import sys
-import os
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
 import tensorflow as tf
 
 import tensorflow.keras.layers as kl
@@ -13,17 +7,23 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+import argparse as arg
+import sys
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
 # Super-resolution Image Generator
 class Generator(tf.keras.Model):
     def __init__(self, input_shape):
         super().__init__()
 
-        input_shape_ps = (input_shape[0], input_shape[1], 64) 
+        input_shape_ps = (input_shape[0], input_shape[1], 64)
 
         # Pre stage(Down Sampling)
         self.pre = [
-            kl.Conv2D(64, kernel_size=9, strides=1,
-                    padding="same", input_shape=input_shape),
+            kl.Conv2D(64, kernel_size=9, strides=1, padding="same", input_shape=input_shape),
             kl.Activation(tf.nn.relu)
         ]
 
@@ -41,7 +41,7 @@ class Generator(tf.keras.Model):
         ]
 
         # Pixel Shuffle(Up Sampling)
-        self.ps =[
+        self.ps = [
             [
                 Pixel_shuffler(128, input_shape_ps) for _ in range(2)
             ],
@@ -49,7 +49,6 @@ class Generator(tf.keras.Model):
         ]
 
     def call(self, x):
-
         # Pre stage
         pre = x
         for layer in self.pre:
@@ -58,8 +57,8 @@ class Generator(tf.keras.Model):
         # Residual Block
         res = pre
         for layer in self.res:
-            for l in layer:
-                res = l(res)
+            for _layer in layer:
+                res = _layer(res)
         
         # Middle stage
         middle = res
@@ -71,54 +70,55 @@ class Generator(tf.keras.Model):
         out = middle
         for layer in self.ps:
             if isinstance(layer, list):
-                for l in layer:
-                    out = l(out)
+                for _layer in layer:
+                    out = _layer(out)
             else:
                 out = layer(out)
 
         return out
 
-# Discriminator 
+
+# Discriminator
 class Discriminator(tf.keras.Model):
     def __init__(self, input_shape):
         super().__init__()
 
         self.conv1 = kl.Conv2D(64, kernel_size=3, strides=1,
-                            padding="same", input_shape=input_shape)
+                                padding="same", input_shape=input_shape)
         self.act1 = kl.Activation(tf.nn.relu)
 
         self.conv2 = kl.Conv2D(64, kernel_size=3, strides=2,
-                            padding="same")
+                                padding="same")
         self.bn1 = kl.BatchNormalization()
         self.act2 = kl.LeakyReLU()
 
         self.conv3 = kl.Conv2D(128, kernel_size=3, strides=1,
-                            padding="same")
+                                padding="same")
         self.bn2 = kl.BatchNormalization()
         self.act3 = kl.LeakyReLU()
 
         self.conv4 = kl.Conv2D(128, kernel_size=3, strides=2,
-                            padding="same")
+                                padding="same")
         self.bn3 = kl.BatchNormalization()
         self.act4 = kl.LeakyReLU()
 
         self.conv5 = kl.Conv2D(256, kernel_size=3, strides=1,
-                            padding="same")
+                                padding="same")
         self.bn4 = kl.BatchNormalization()
         self.act5 = kl.LeakyReLU()
 
         self.conv6 = kl.Conv2D(256, kernel_size=3, strides=2,
-                            padding="same")
+                                padding="same")
         self.bn5 = kl.BatchNormalization()
         self.act6 = kl.LeakyReLU()
 
         self.conv7 = kl.Conv2D(512, kernel_size=3, strides=1,
-                            padding="same")
+                                padding="same")
         self.bn6 = kl.BatchNormalization()
         self.act7 = kl.LeakyReLU()
 
         self.conv8 = kl.Conv2D(512, kernel_size=3, strides=2,
-                            padding="same")
+                                padding="same")
         self.bn7 = kl.BatchNormalization()
         self.act8 = kl.LeakyReLU()
 
@@ -128,7 +128,6 @@ class Discriminator(tf.keras.Model):
         self.dens2 = kl.Dense(1, activation="sigmoid")
 
     def call(self, x):
-
         d1 = self.act1(self.conv1(x))
         d2 = self.act2(self.bn1(self.conv2(d1)))
         d3 = self.act3(self.bn2(self.conv3(d2)))
@@ -143,22 +142,23 @@ class Discriminator(tf.keras.Model):
 
         return d10
 
+
 # Pixel Shuffle
 class Pixel_shuffler(tf.keras.Model):
     def __init__(self, out_ch, input_shape):
         super().__init__()
 
         self.conv = kl.Conv2D(out_ch, kernel_size=3, strides=1,
-                            padding="same", input_shape=input_shape)
+                                padding="same", input_shape=input_shape)
         self.act = kl.Activation(tf.nn.relu)
     
     # forward proc
     def call(self, x):
-
         d1 = self.conv(x)
         d2 = self.act(tf.nn.depth_to_space(d1, 2))
 
         return d2
+
 
 # Residual Block
 class Res_block(tf.keras.Model):
@@ -166,35 +166,34 @@ class Res_block(tf.keras.Model):
         super().__init__()
 
         self.conv1 = kl.Conv2D(ch, kernel_size=3, strides=1,
-                            padding="same", input_shape=input_shape)
+                                padding="same", input_shape=input_shape)
         self.bn1 = kl.BatchNormalization()
         self.av1 = kl.Activation(tf.nn.relu)
 
         self.conv2 = kl.Conv2D(ch, kernel_size=3, strides=1,
-                            padding="same")
+                                padding="same")
         self.bn2 = kl.BatchNormalization()
 
         self.add = kl.Add()
 
     def call(self, x):
-
         d1 = self.av1(self.bn1(self.conv1(x)))
         d2 = self.bn2(self.conv2(d1))
 
         return self.add([x, d2])
 
+
 # Train
 class trainer():
     def __init__(self, lr_img, hr_img):
-
-        lr_shape = lr_img.shape
-        hr_shape = hr_img.shape
+        lr_shape = lr_img.shape  # Low-resolution Image shape
+        hr_shape = hr_img.shape  # High-resolution Image shape
 
         # Content Loss Model setup
         input_tensor = tf.keras.Input(shape=hr_shape)
         self.vgg = VGG16(include_top=False, input_tensor=input_tensor)
         self.vgg.trainable = False
-        self.vgg.outputs = [self.vgg.layers[9].output]  # VGG16 block3_conv3 output  
+        self.vgg.outputs = [self.vgg.layers[9].output]  # VGG16 block3_conv3 output
 
         # Content Loss Model
         self.cl_model = tf.keras.Model(input_tensor, self.vgg.outputs)
@@ -205,8 +204,8 @@ class trainer():
         outputs = discriminator_(inputs)
         self.discriminator = tf.keras.Model(inputs=inputs, outputs=outputs)
         self.discriminator.compile(optimizer=tf.keras.optimizers.Adam(),
-                                loss=tf.keras.losses.BinaryCrossentropy(),
-                                metrics=['accuracy'])
+                                    loss=tf.keras.losses.BinaryCrossentropy(),
+                                    metrics=['accuracy'])
 
         # Generator
         self.generator = Generator(lr_shape)
@@ -215,13 +214,14 @@ class trainer():
         lr_input = tf.keras.Input(shape=lr_shape)
         sr_output = self.generator(lr_input)
 
-        self.discriminator.trainable = False
+        self.discriminator.trainable = False  # Discriminator train Disable
         d_fake = self.discriminator(sr_output)
 
+        # SRGAN Model
         self.gan = tf.keras.Model(inputs=lr_input, outputs=[sr_output, d_fake])
         self.gan.compile(optimizer=tf.keras.optimizers.Adam(),
-                        loss=[self.Content_loss, tf.keras.losses.BinaryCrossentropy()],
-                        loss_weights=[1., 1e-3])
+                            loss=[self.Content_loss, tf.keras.losses.BinaryCrossentropy()],
+                            loss_weights=[1., 1e-3])
         
     # Content loss
     def Content_loss(self, hr_img, sr_img):
@@ -232,7 +232,6 @@ class trainer():
         return cv2.PSNR(hr_img, sr_img)
         
     def train(self, lr_imgs, hr_imgs, out_path, batch_size, epoch):
-
         g_loss_plt = []
         d_loss_plt = []
         path = os.path.join(out_path, "graph.jpg")
@@ -240,13 +239,12 @@ class trainer():
 
         h_batch = int(batch_size / 2)
 
-        real_lab = np.ones((h_batch, 1))  # High-resolution image label
-        fake_lab = np.zeros((h_batch, 1)) # Super-resolution image label(Discriminator side)
+        real_lab = np.ones((h_batch, 1))   # High-resolution image label
+        fake_lab = np.zeros((h_batch, 1))  # Super-resolution image label(Discriminator side)
         gan_lab = np.ones((h_batch, 1))
 
         # train run
         for epoch in range(epoch):
-
             # - Train Discriminator -
 
             # High-resolution image random pickups
@@ -263,10 +261,10 @@ class trainer():
             d_real_loss = self.discriminator.train_on_batch(hr_img, real_lab)
 
             # train by Super-resolution image
-            sr_img = self.generator.predict(lr_img) 
+            sr_img = self.generator.predict(lr_img)
             d_fake_loss = self.discriminator.train_on_batch(sr_img, fake_lab)
             
-            # Discriminator average loss 
+            # Discriminator average loss
             d_loss = 0.5 * np.add(d_real_loss, d_fake_loss)
             
             # - Train Generator -
@@ -283,13 +281,13 @@ class trainer():
             g_loss = self.gan.train_on_batch(lr_img, [hr_img, gan_lab])
 
             # Epoch num, Discriminator/Generator loss, PSNR
-            print("Epoch: {0} D_loss: {1:.3f} G_loss: {2:.3f} PSNR: {3:.3f}".format(epoch+1, d_loss[0], g_loss[0], self.psnr(hr_img, sr_img)))
+            print("Epoch: {0} D_loss: {1:.3f} G_loss: {2:.3f} PSNR: {3:.3f}".format(epoch + 1, d_loss[0], g_loss[0], self.psnr(hr_img, sr_img)))
 
             d_loss_plt.append(d_loss[0])
             g_loss_plt.append(g_loss[0])
 
             # Plotting the loss value
-            if (epoch+1) % 50 == 0:
+            if (epoch + 1) % 50 == 0:
                 plt.plot(d_loss_plt)
                 plt.plot(g_loss_plt)
                 plt.savefig(path, bbox_inches='tight', pad_inches=0.1)
@@ -299,14 +297,15 @@ class trainer():
         # Parameter-File and Graph Saving
         print("___Saving parameter...")
         self.generator.save_weights(os.path.join(out_path, "srgan.h5"))
+        
         plt.plot(d_loss_plt, label="D_loss")
         plt.plot(g_loss_plt, label="G_loss")
         plt.savefig(path, bbox_inches='tight', pad_inches=0.1)
         print("___Successfully completed\n\n")
 
+
 # Dataset creation
 def create_dataset(data_dir, h, w, mag):
-
     print("\n___Creating a dataset...")
     
     prc = ['/', '-', '\\', '|']
@@ -325,22 +324,22 @@ def create_dataset(data_dir, h, w, mag):
 
         img = cv2.imread(d)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = cv2.resize(img, (h, w)) # High-resolution image
+        img = cv2.resize(img, (h, w))  # High-resolution image
 
-        img_low = cv2.resize(img, (int(h/mag), int(w/mag))) # Image reduction
-        img_low = cv2.resize(img_low, (h, w)) # Resize to original size
+        img_low = cv2.resize(img, (int(h / mag), int(w / mag)))  # Image reduction
+        img_low = cv2.resize(img_low, (h, w))  # Resize to original size
 
         lr_imgs.append(img_low)
         hr_imgs.append(img)
 
         cnt += 1
 
-        print("\rLoading a LR-images and HR-images...{}    ({} / {})".format(prc[cnt%4], cnt, len(os.listdir(data_dir))), end='')
+        print("\rLoading a LR-images and HR-images...{}    ({} / {})".format(prc[cnt % 4], cnt, len(os.listdir(data_dir))), end='')
 
     print("\rLoading a LR-images and HR-images...Done    ({} / {})".format(cnt, len(os.listdir(data_dir))), end='')
 
     # Low-resolution image
-    lr_imgs = tf.convert_to_tensor(lr_imgs, np.float32) 
+    lr_imgs = tf.convert_to_tensor(lr_imgs, np.float32)
     lr_imgs = (lr_imgs.numpy() - 127.5) / 127.5
 
     # High-resolution image
@@ -351,8 +350,8 @@ def create_dataset(data_dir, h, w, mag):
 
     return lr_imgs, hr_imgs
 
-def main():
 
+def main():
     # Command line option
     parser = arg.ArgumentParser(description='Super-resolution GAN training')
     parser.add_argument('--data_dir', '-d', type=str, default=None,
@@ -365,19 +364,19 @@ def main():
     parser.add_argument('--epoch', '-e', type=int, default=1000,
                         help='Specify the number of times to train (default: 1000)')
     parser.add_argument('--he', '-he', type=int, default=128,
-                        help='Resize height (default: 128)')      
+                        help='Resize height (default: 128)')
     parser.add_argument('--wi', '-wi', type=int, default=128,
                         help='Resize width (default: 128)')
     parser.add_argument('--mag', '-m', type=int, default=2,
-                        help='Magnification (default: 2)')                           
+                        help='Magnification (default: 2)')
     args = parser.parse_args()
 
     # Image folder not specified. -> Exception
-    if args.data_dir == None:
+    if args.data_dir is None:
         print("\nException: Folder not specified.\n")
         sys.exit()
     # An image folder that does not exist was specified. -> Exception
-    if os.path.exists(args.data_dir) != True:
+    if os.path.exists(args.data_dir) is False:
         print("\nException: Folder \"{}\" is not found.\n".format(args.data_dir))
         sys.exit()
     # When 0 is entered for either width/height or Reduction ratio. -> Exception
@@ -406,6 +405,7 @@ def main():
     print("___Start training...")
     Trainer = trainer(lr_imgs[0], hr_imgs[0])
     Trainer.train(lr_imgs, hr_imgs, out_path=args.out, batch_size=args.batch_size, epoch=args.epoch)
+
 
 if __name__ == '__main__':
     main()
